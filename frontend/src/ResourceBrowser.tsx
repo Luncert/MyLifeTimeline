5762f35ef5a4785c777ef7a79bdade76f4f2142e
Luncert <2725115515@qq.com>
2023-12-22 11:37:07 +0800

    add file tree

--- frontend/src/ResourceBrowser.tsx
+++ frontend/src/ResourceBrowser.tsx
@@ -1,9 +1,11 @@
-import { Breadcrumbs, Button, ButtonGroup, Grid, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@suid/material";
+import { Breadcrumbs, Button, ButtonGroup, Divider, Grid, IconButton, InputAdornment, Link, Stack, TextField, Typography } from "@suid/material";
 import { IoArrowBackOutline, IoArrowForwardOutline } from 'solid-icons/io';
 import { BiRegularSearchAlt } from 'solid-icons/bi';
-import { For, createContext } from "solid-js";
+import { For, Match, Show, Switch, createContext, createResource } from "solid-js";
 import { createBucket, useCtx } from "./mgrui/lib/components/utils";
 import { AiTwotoneFileText } from 'solid-icons/ai';
+import { FaSolidAngleDown, FaSolidAngleRight, FaRegularFolderOpen, FaRegularFolderClosed } from 'solid-icons/fa';
+import { IoFolderOpen, IoFolder } from 'solid-icons/io';
 
 interface File {
   name: string;
@@ -12,7 +14,75 @@ interface File {
   preview?: string;
 }
 
+interface FileNode {
+  name: string;
+  mediaType: string;
+}
+
+interface DirectoryNode {
+  name: string;
+  children: (DirectoryNode | FileNode)[];
+}
+
+type FileTree = (DirectoryNode | FileNode)[];
+
+const fileTree: FileTree = [
+  {
+    name: "app",
+    children: [
+      {
+        name: "config.ts",
+        mediaType: "text"
+      }
+    ]
+  },
+  {
+    name: "lib",
+    children: []
+  },
+  {
+    name: "Divider.tsx",
+    mediaType: "text"
+  }
+];
+
 const files: File[] = [
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
+  {
+    name: "a.png",
+    mediaType: "image/png"
+  },
   {
     name: "a.png",
     mediaType: "image/png"
@@ -49,51 +119,111 @@ export default function ResourceBrowser() {
     <ResourceBrowserContext.Provider value={{
       path, currentPath
     }}>
-      <div class="w-full h-full p-1">
-        <Stack class="h-10 gap-x-1" direction="row">
-          <ButtonGroup class="shrink-0 gap-1">
-            <IconButton sx={{ borderRadius: 2 }} onClick={() => {
-              currentPath(Math.max(0, currentPath() - 1));
-            }}>
-              <IoArrowBackOutline />
-            </IconButton>
-            <IconButton sx={{ borderRadius: 2 }} onClick={() => {
-              currentPath(Math.min(path().length - 1, currentPath() + 1));
-            }}>
-              <IoArrowForwardOutline />
-            </IconButton>
-            <Breadcrumbs class="shrink-0 rounded-md bg-zinc-200" aria-label="breadcrumb" sx={{
-            }}>
-              <For each={path().filter((v, i) => i <= currentPath())}>{(item, idx) => (
-                <IconButton size="small" sx={{ borderRadius: 2, paddingLeft: 2, paddingRight: 2 }}
-                  onClick={() => currentPath(idx())}>
-                  {item}
-                </IconButton>
-              )}</For>
-            </Breadcrumbs>
-          </ButtonGroup>
-
-          <TextField class="w-full" size="small" autoComplete="none" InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton edge="end" sx={{ borderRadius: 2 }}>
-                  <BiRegularSearchAlt />
-                </IconButton>
-              </InputAdornment>
-            )
-          }} />
-        </Stack>
-        <div class="w-full h-full shrink">
-          <Grid container spacing={2}>
-            <For each={files}>{file => (
-              <Grid item xs={8} md={1}>
-                <FileElement file={file} />
-              </Grid>
-            )}</For>
-          </Grid>
-        </div>
+      <div class="w-full h-full p-1 flex">
+        <FileTree />
+        <Divider orientation="vertical" />
+        <CurrentPathBrowser />
       </div>
     </ResourceBrowserContext.Provider>
+  )
+}
+
+function FileTree() {
+  return (
+    <div class="flex flex-col gap-1 p-1 w-1/3">
+      <TextField class="w-full" size="small" autoComplete="none" InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton edge="end" sx={{ borderRadius: 2 }}>
+              <BiRegularSearchAlt />
+            </IconButton>
+          </InputAdornment>
+        )
+      }} />
+      <For each={["x", "y", "z"]}>{item => (
+        <FileTreeNode basePath="/" name={item} mediaType="directory" />
+      )}</For>
+    </div>
+  )
+}
+
+function FileTreeNode(props: {
+  basePath: string;
+  name: string;
+  mediaType: string;
+}) {
+  const expanded = createBucket(false);
+  const isDirectory = props.mediaType === "directory";
+  createResource(() => "");
+  return (
+    <div>
+      <Button fullWidth color="inherit" size="small" startIcon={
+        <div class="inline-block flex gap-1">
+          <Show when={isDirectory} fallback={<AiTwotoneFileText />}>
+            <Show when={expanded()} fallback={
+              <>
+                <FaSolidAngleRight />
+                <IoFolder />
+              </>
+            }>
+              <FaSolidAngleDown />
+              <IoFolderOpen />
+            </Show>
+          </Show>
+        </div>
+      } sx={{justifyContent: "start"}} onClick={() => {
+        expanded(!expanded());
+      }}>
+        <span>{props.name}</span>
+      </Button>
+      <Show when={isDirectory}>
+        <div class="ml-[1.4rem]">
+          <For each={["x", "y", "z"]}>{item => (
+            <FileTreeNode basePath={props.basePath + "/" + props.name} name={item} mediaType="file" />
+          )}</For>
+        </div>
+      </Show>
+    </div>
+  );
+}
+
+function CurrentPathBrowser() {
+  const ctx = useResourceBrowser();
+  return (
+    <div class="flex flex-col w-full h-full shrink">
+      <Stack class="p-1 gap-x-1" direction="row">
+        <Breadcrumbs class="shrink-0" aria-label="breadcrumb" sx={{
+        }}>
+          <For each={ctx.path().filter((v, i) => i <= ctx.currentPath())}>{(item, idx) => (
+            <IconButton disabled={idx() === ctx.currentPath()} size="small" sx={{ borderRadius: 2, paddingLeft: 2, paddingRight: 2 }}
+              onClick={() => ctx.currentPath(idx())}>
+              {item}
+            </IconButton>
+          )}</For>
+        </Breadcrumbs>
+
+        <ButtonGroup class="ml-auto shrink-0 gap-1">
+          <IconButton sx={{ borderRadius: 2 }} onClick={() => {
+            ctx.currentPath(Math.max(0, ctx.currentPath() - 1));
+          }}>
+            <IoArrowBackOutline />
+          </IconButton>
+          <IconButton sx={{ borderRadius: 2 }} onClick={() => {
+            ctx.currentPath(Math.min(ctx.path().length - 1, ctx.currentPath() + 1));
+          }}>
+            <IoArrowForwardOutline />
+          </IconButton>
+        </ButtonGroup>
+      </Stack>
+      <Divider />
+      <div class="w-full h-full shrink p-1 overflow-y-auto">
+        <div class="flex flex-wrap gap-1">
+          <For each={files}>{file => (
+            <FileElement file={file} />
+          )}</For>
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -101,9 +231,16 @@ function FileElement(props: {
   file: File;
 }) {
   return (
-    <div class="w-full h-full">
-      <AiTwotoneFileText class="text-5xl" />
-      {props.file.name}
-    </div>
+    <IconButton color="info" sx={{
+      flexShrink: 0,
+      borderRadius: 1.5,
+      width: "5rem",
+      height: "5rem"
+    }}>
+      <div class="flex flex-col items-center">
+        <AiTwotoneFileText class="text-5xl" />
+        <span class="text-sm text-zinc-900">{props.file.name}</span>
+      </div>
+    </IconButton>
   )
 }
