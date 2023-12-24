f1f1a9081f97837fb085f42bb88a574385652d2e
Luncert <2725115515@qq.com>
2023-12-24 21:27:01 +0800

    update

--- frontend/src/views/gallery/GalleryWithTimeline.tsx
+++ frontend/src/views/gallery/GalleryWithTimeline.tsx
@@ -2,19 +2,27 @@ import { Fab } from "@suid/material";
 import Timeline from "./Timeline";
 import { IoAdd } from 'solid-icons/io';
 import { FiEdit2 } from 'solid-icons/fi';
+import ScrollBox from "../../mgrui/lib/components/ScrollBox";
 
 export default function GalleryWithTimeline() {
   return (
     <div class="w-full h-full flex">
-      <Timeline />
-      <div class="absolute bottom-4 right-4 flex flex-col gap-2">
-        <Fab color="info" aria-label="add" size="small">
-          <IoAdd />
-        </Fab>
-        <Fab color="info" aria-label="edit" size="small">
-          <FiEdit2 />
-        </Fab>
-      </div>
+      <ScrollBox>
+        <div id="gallery-with-timeline-show" class="w-full h-full">
+          <Timeline />
+          <div class="absolute bottom-4 right-4 flex flex-col gap-2">
+            <Fab color="info" aria-label="add" size="small">
+              <IoAdd />
+            </Fab>
+            <Fab color="info" aria-label="edit" size="small">
+              <FiEdit2 />
+            </Fab>
+          </div>
+        </div>
+        <div id="gallery-with-timeline-edit">
+
+        </div>
+      </ScrollBox>
     </div>
   )
 }
