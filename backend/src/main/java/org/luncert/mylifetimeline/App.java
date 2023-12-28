713e753f89ca28afd170d9bf5089ec39e793e975
Luncert <2725115515@qq.com>
2023-12-28 11:54:03 +0800

    remove usb

--- backend/src/main/java/org/luncert/mylifetimeline/App.java
+++ backend/src/main/java/org/luncert/mylifetimeline/App.java
@@ -1,5 +1,6 @@
 package org.luncert.mylifetimeline;
 
+import net.codecrete.usb.USB;
 import org.luncert.mylifetimeline.model.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class App {
 
   public static void main(String[] args) {
+    for (var device : USB.getAllDevices()) {
+      System.out.println(device);
+    }
     SpringApplication.run(App.class, args);
   }
 }
